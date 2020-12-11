b7141fdad2b0e98dd98c2ab0114680bd9a44ee8c
chrismilleruk <chris@blinkbox.com>
2020-12-11 09:24:41 +0000

    Day 4: Passport Processing (WIP)

---
+++ 04.py
@@ -0,0 +1,5 @@
+# Day 4: Passport Processing 
+
+# Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+def part1(data):
+  return None
