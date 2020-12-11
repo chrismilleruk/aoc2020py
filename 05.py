d8cc94abb1dd38e0f5045ed827511dbb3f91f1b6
chrismilleruk <chris@blinkbox.com>
2020-12-11 11:57:12 +0000

    Day 5: Binary Boarding (WIP)

---
+++ 05.py
@@ -0,0 +1,23 @@
+
+
+def part1(data):
+  part1_test(part1_fn)
+  print('self-check tests PASS')
+  return part1_fn(data)
+
+def part1_fn(data):
+  return None
+
+def part1_test(data):
+  return None
+
+def part2(data):
+  part2_test(part2_fn)
+  print('part2 self-check tests PASS')
+  return part2_fn(data)
+
+def part2_fn(data):
+  return None
+
+def part2_test(data):
+  return None
