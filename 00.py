c7b25e99e86b849f03f84646d512b50ee8e4afe2
chrismilleruk <chris@blinkbox.com>
2020-12-11 15:18:12 +0000

    pytest: Update template

--- 00.py
+++ 00.py
@@ -1,22 +1,9 @@
 
 def part1(data):
-  part1_test(part1_fn)
-  print('self-check tests PASS')
-  return part1_fn(data)
-
-def part1_fn(data):
   return None
 
-def part1_test(data):
-  return None
+def test_part1():
+  assert True is True
 
 def part2(data):
-  part2_test(part2_fn)
-  print('self-check tests PASS')
-  return part2_fn(data)
-
-def part2_fn(data):
-  return None
-
-def part2_test(data):
   return None
