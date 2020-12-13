7992d3a372b7546335a7c40c75b0f529aa0e314a
chrismilleruk <chrismilleruk@gmail.com>
2020-12-13 20:47:17 +0000

    Day 11: Seating System (setup)

---
+++ 11.py
@@ -0,0 +1,27 @@
+# Day 11: Seating System
+import pytest
+
+def part1(data):
+  return None
+
+def part2(data):
+  return None
+
+
+def test_part1(example1):
+  assert part1(example1) is None
+
+@pytest.fixture
+def example1():
+  return """L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"""
+
+
