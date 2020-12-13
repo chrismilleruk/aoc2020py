068587756a2eb3c8bfe858a68068b4cbb79be987
chrismilleruk <chrismilleruk@gmail.com>
2020-12-13 18:25:09 +0000

    Update 00.py

--- 00.py
+++ 00.py
@@ -1,9 +1,17 @@
+import pytest
 
 def part1(data):
   return None
 
-def test_part1():
-  assert True is True
-
 def part2(data):
   return None
+
+
+def test_part1(example1):
+  assert part1(example1) is None
+
+@pytest.fixture
+def example1():
+  return """35
+20
+15"""
