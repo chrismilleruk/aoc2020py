99b3fe5dc2bd27b4220c5abc9fa30c25d1c993fd
chrismilleruk <chrismilleruk@gmail.com>
2020-12-14 15:47:58 +0000

    Day 13: Shuttle Search (Pt 1)

--- 13.py
+++ 13.py
@@ -3,7 +3,15 @@ import pytest
 
 # What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus?
 def part1(data):
-  return 295
+  lines = data.split('\n')
+  now = int(lines[0])
+  buses = list(map(int, [x for x in lines[1].split(',') if x != 'x']))
+  print(now, buses)
+  wait_times = list(map(lambda x: x - (now % x), buses))
+  best_bus_wait = min(wait_times)
+  best_bus_id = buses[wait_times.index(best_bus_wait)]
+  print(list(wait_times), best_bus_wait, best_bus_id)
+  return best_bus_id * best_bus_wait
 
 # The earliest bus you could take is bus ID 59. It doesn't depart until timestamp 944, so you would need to wait 944 - 939 = 5 minutes before it departs. Multiplying the bus ID by the number of minutes you'd need to wait gives 295.
 def test_part1(example1):
