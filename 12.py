eea7a79e86cb441e502c762236d3835beffcb930
chrismilleruk <chrismilleruk@gmail.com>
2020-12-14 14:44:29 +0000

    Day 12: Rain Risk (Pt 1)

--- 12.py
+++ 12.py
@@ -3,7 +3,34 @@ import pytest
 
 # Figure out where the navigation instructions lead. What is the Manhattan distance between that location and the ship's starting position?
 def part1(data):
-  return 25
+  pos = [0, 0]
+  dir = ['E', 'S', 'W', 'N']
+  for instruction in data.split('\n'):
+    command = instruction[0]
+    number = int(instruction[1:])
+    
+    if command == 'L' or command == 'R':
+      turns = (number // 90) % 4
+
+      if command == 'L': turns = 4 - turns
+      dir = dir[turns:] + dir[:turns]
+      # print(instruction, command, turns, pos, dir)
+      continue
+
+    if command == 'F':
+      command = dir[0]
+    
+    if command == 'W' or command == 'S':
+      number = -number 
+
+    if command == 'E' or command == 'W':
+      pos[0] += number 
+    if command == 'N' or command == 'S':
+      pos[1] += number 
+    
+    # print(instruction, command, number, pos, dir[0])
+
+  return sum(map(abs, pos))
 
 
 def test_part1(example1):
