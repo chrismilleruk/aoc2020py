fc7bfb8b235f2ddccc8aa6de367bbf7ff195fd6b
chrismilleruk <chrismilleruk@gmail.com>
2020-12-18 15:03:42 +0000

    Day 18: Operation Order (Pt 2)

--- 18.py
+++ 18.py
@@ -9,20 +9,28 @@ def part1(data):
     tokens = tokenize(line)
     program = parse(tokens)
     val = calc(program)
-    print(val, '=', line)
+    # print(val, '=', line)
     total += val
 
   return total
 
 # Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
 def part2(data):
-  return None
+  total = 0
+  for line in data.split('\n'):
+    tokens = tokenize(line)
+    program = parse(tokens)
+    val = calc2(program)
+    # print(val, '=', line)
+    total += val
+
+  return total
 
 def tokenize(line):
   match = re.findall(r'\d+|\+|\*|\(|\)', line)
   return match
   
-def parse(tokens, state = {'level': 0, 'pos': 0}):
+def parse(tokens):
   operators = ['+', '*']
   stack = [[]]
 
@@ -54,12 +62,28 @@ def calc(arr):
 
     if op == '+':
       total += val
-    elif op == '-':
-      total -= val
     elif op == '*':
       total *= val
-    elif op == '/':
-      total /= val
+  return total
+
+
+def calc2(arr):
+  if isinstance(arr, int):
+    return arr
+
+  if '+' in arr and '*' in arr:
+    # addition is evaluated before multiplication.
+    split = arr.index('*')
+    return calc2(arr[0:split]) * calc2(arr[split+1:])
+
+  total = calc2(arr[0])
+  for op, val in zip(arr[1::2], arr[2::2]):
+    val = calc2(val)
+
+    if op == '+':
+      total += val
+    elif op == '*':
+      total *= val
   return total
 
 
@@ -79,7 +103,7 @@ def test_calc():
   # # ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 13632.
   # assert calc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
 
-def ntest_part1():
+def test_part1():
   # For example, the steps to evaluate the expression 1 + 2 * 3 + 4 * 5 + 6 are as follows:
 
   # 1 + 2 * 3 + 4 * 5 + 6
@@ -108,6 +132,33 @@ def ntest_part1():
   assert part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
   # ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 13632.
   assert part1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
+
+def test_part2():
+  # Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
+
+  # For example, the steps to evaluate the expression 1 + 2 * 3 + 4 * 5 + 6 are now as follows:
+
+  # 1 + 2 * 3 + 4 * 5 + 6
+  #   3   * 3 + 4 * 5 + 6
+  #   3   *   7   * 5 + 6
+  #   3   *   7   *  11
+  #      21       *  11
+  #          231
+  assert part2("1 + 2 * 3 + 4 * 5 + 6") == 231
+  
+  # Here are the other examples from above:
+
+  # 1 + (2 * 3) + (4 * (5 + 6)) still becomes 51.
+  assert part2("1 + (2 * 3) + (4 * (5 + 6))") == 51
+  # 2 * 3 + (4 * 5) becomes 46.
+  assert part2("2 * 3 + (4 * 5)") == 46
+  # 5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 1445.
+  assert part2("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+  # 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 669060.
+  assert part2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669060
+  # ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 23340.
+  assert part2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
+
 
 @pytest.fixture
 def example1():
